0c473a681657707117f8e16a25bde8ffe8618d1c
4ra1n <2023503307@qq.com>
2023-10-24 16:57:03 +0800

    linux sh script

---
+++ build/start-system.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# set file name
+jar_analyzer="jar-analyzer-2.1-beta.jar"
+
+# env
+command="free -m | awk 'NR==2{print \$7}'"
+cur_dir="$(dirname "$0")"
+jar_file="lib/$jar_analyzer"
+jar_file_abs="$cur_dir/$jar_file"
+
+# get free memory
+m=$(eval $command)
+
+# use 2/3 free memory
+heapsize=$((m * 2 / 3))
+
+# jvm args
+gc_args="-XX:+PrintGC -XX:+PrintGCTimeStamps"
+other_args="-Dfile.encoding=UTF-8"
+java_args="$gc_args -Xmx${heapsize}M -Xms${heapsize}M $other_args"
+
+# start jar
+java $java_args -jar "$jar_file_abs"

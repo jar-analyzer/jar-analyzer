bcb8c5fc4016c0cf52bc073fddc1239d51f962ad
4ra1n <2023503307@qq.com>
2023-12-01 10:56:12 +0800

    update main

--- src/main/java/me/n1ar4/jar/analyzer/starter/Application.java
+++ src/main/java/me/n1ar4/jar/analyzer/starter/Application.java
@@ -2,6 +2,7 @@ package me.n1ar4.jar.analyzer.starter;
 
 import me.n1ar4.jar.analyzer.gui.MainForm;
 import me.n1ar4.jar.analyzer.gui.util.JarAnalyzerLaf;
+import me.n1ar4.log.LogLevel;
 import me.n1ar4.log.LogManager;
 import me.n1ar4.log.Logger;
 import me.n1ar4.log.LoggingStream;
@@ -9,20 +10,40 @@ import me.n1ar4.log.LoggingStream;
 public class Application {
     private static final Logger logger = LogManager.getLogger();
 
+    /**
+     * Main Method
+     * 　　 へ　　　　　／|
+     * 　　/＼7　　　 ∠＿/
+     * 　 /　│　　 ／　／
+     * 　│　Z ＿,＜　／　　 /`ヽ
+     * 　│　　　　　ヽ　　 /　　〉
+     * 　 Y　　　　　`　 /　　/
+     * 　?●　?　●　　??〈　　/
+     * 　()　 へ　　　　|　＼〈
+     * 　　>? ?_　 ィ　 │ ／／
+     * 　 / へ　　 /　?＜| ＼＼
+     * 　 ヽ_?　　(_／　 │／／
+     * 　　7　　　　　　　|／
+     * 　　＞―r￣￣~∠--|
+     */
     public static void main(String[] args) {
+        // SET LOG LEVEL
+        LogManager.setLevel(LogLevel.INFO);
+        // PRINT LOGO
         Logo.print();
         try {
+            // SET LOOK AND FEEL
             if (JarAnalyzerLaf.setup()) {
                 logger.info("setup look and feel success");
             }
-
+            // CHECK SINGLE INSTANCE
             if (!Single.canRun()) {
                 System.exit(0);
             }
-
+            // REDIRECT SYSTEM OUT
             System.setOut(new LoggingStream(System.out, logger));
             System.out.println("set log42j io-streams");
-
+            // START GUI
             MainForm.start();
         } catch (Exception ex) {
             logger.error("start jar analyzer error: {}", ex.toString());

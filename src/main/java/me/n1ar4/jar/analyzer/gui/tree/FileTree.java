f89d7cf1ff867879a833af529ef02e2b61b2ddd6
Honey Baby 哈利·卑鄙 <1905411470@qq.com>
2024-07-20 21:35:59 +0800

    【ADD】定位类搜索结果 【FIX】点击展开箭头时高亮无法消失

--- src/main/java/me/n1ar4/jar/analyzer/gui/tree/FileTree.java
+++ src/main/java/me/n1ar4/jar/analyzer/gui/tree/FileTree.java
@@ -1,11 +1,15 @@
 package me.n1ar4.jar.analyzer.gui.tree;
 
+import cn.hutool.core.util.ArrayUtil;
+import cn.hutool.core.util.StrUtil;
 import me.n1ar4.jar.analyzer.starter.Const;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.event.TreeExpansionEvent;
 import javax.swing.event.TreeExpansionListener;
+import javax.swing.event.TreeSelectionEvent;
+import javax.swing.event.TreeSelectionListener;
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.DefaultTreeCellRenderer;
 import javax.swing.tree.DefaultTreeModel;
@@ -13,31 +17,29 @@ import javax.swing.tree.TreePath;
 import java.awt.*;
 import java.io.File;
 import java.util.Arrays;
+import java.util.Enumeration;
 import java.util.List;
 import java.util.Objects;
 
 public class FileTree extends JTree {
-    protected DefaultMutableTreeNode rootNode;
-    protected DefaultTreeModel fileTreeModel;
-    private final DefaultTreeModel savedModel;
     private static ImageIcon classIcon;
 
     static {
         try {
-            classIcon = new ImageIcon(ImageIO.read(
-                    Objects.requireNonNull(FileTree.class
-                            .getClassLoader().getResourceAsStream("img/class.png"))));
+            classIcon = new ImageIcon(ImageIO.read(Objects.requireNonNull(FileTree.class.getClassLoader().getResourceAsStream("img/class.png"))));
         } catch (Exception ignored) {
         }
     }
+
+    private final DefaultTreeModel savedModel;
+    protected DefaultMutableTreeNode rootNode;
+    protected DefaultTreeModel fileTreeModel;
 
     public FileTree() {
         savedModel = (DefaultTreeModel) this.getModel();
         DefaultTreeCellRenderer renderer = new DefaultTreeCellRenderer() {
             @Override
-            public Component getTreeCellRendererComponent(JTree tree, Object value,
-                                                          boolean sel, boolean expanded,
-                                                          boolean leaf, int row, boolean hasFocus) {
+            public Component getTreeCellRendererComponent(JTree tree, Object value, boolean sel, boolean expanded, boolean leaf, int row, boolean hasFocus) {
                 super.getTreeCellRendererComponent(tree, value, sel, expanded, leaf, row, hasFocus);
                 if (leaf && value instanceof DefaultMutableTreeNode) {
                     DefaultMutableTreeNode node = (DefaultMutableTreeNode) value;
@@ -78,11 +80,27 @@ public class FileTree extends JTree {
     }
 
     private void initListeners() {
+        addTreeSelectionListener(new TreeSelectionListener() {
+
+            @Override
+            public void valueChanged(TreeSelectionEvent e) {
+                TreePath path = e.getNewLeadSelectionPath();
+                if (path != null) {
+                    Rectangle bounds = getPathBounds(path);
+                    if (bounds != null) {
+                        int height = getHeight() / 3;
+                        Rectangle rectangle = new Rectangle(bounds.x, bounds.y + height, bounds.width, bounds.height);
+                        scrollRectToVisible(rectangle);
+                    }
+                }
+            }
+        });
         addTreeExpansionListener(new TreeExpansionListener() {
             public void treeCollapsed(TreeExpansionEvent event) {
             }
 
             public void treeExpanded(TreeExpansionEvent event) {
+                clearSelection();
                 TreePath path = event.getPath();
                 DefaultMutableTreeNode treeNode = (DefaultMutableTreeNode) path.getLastPathComponent();
                 treeNode.removeAllChildren();
@@ -139,7 +157,42 @@ public class FileTree extends JTree {
                     subNode.add(new DefaultMutableTreeNode("fake"));
                 }
                 node.add(subNode);
+                addSelectionPath(new TreePath(node.getPath()));
             }
         }
+    }
+
+    private void expandPathTarget(Enumeration children1, String[] split) {
+        while (children1.hasMoreElements()) {
+            DefaultMutableTreeNode children = (DefaultMutableTreeNode) children1.nextElement();
+            for (int i = 0; i < split.length - 1; i++) {
+                if (children.toString().equals(split[i])) {
+                    expandPath(new TreePath(children.getPath()));
+                    if (split.length - 2 == i) {
+                        Enumeration children2 = children.children();
+                        while (children2.hasMoreElements()) {
+                            DefaultMutableTreeNode end = (DefaultMutableTreeNode) children2.nextElement();
+                            String var0 = "";
+                            if (split[split.length - 1].contains("$")) {
+                                var0 = StrUtil.subBefore(split[split.length - 1], "$", false);
+                            }
+                            if (end.toString().equals(split[split.length - 1] + ".class") || (StrUtil.isNotEmpty(var0) && end.toString().equals(var0 + ".class"))) {
+                                setSelectionPath(new TreePath(end.getPath()));
+                                return;
+                            }
+                        }
+                    }
+                    expandPathTarget(children.children(), split);
+                }
+            }
+        }
+    }
+
+    public void searchPathTarget(String classname) {
+        refresh();
+        String[] split = classname.split("/");
+        String[] resize = ArrayUtil.resize(split, split.length);
+        Enumeration children1 = rootNode.children();
+        expandPathTarget(children1, split);
     }
 }

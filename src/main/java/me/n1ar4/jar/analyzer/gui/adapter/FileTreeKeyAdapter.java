095662cfd8717a714b565a6dfe209d712496345f
Honey Baby 哈利·卑鄙 <1905411470@qq.com>
2024-07-20 21:37:35 +0800

    【ADD】搜索控件快捷键Listener

---
+++ src/main/java/me/n1ar4/jar/analyzer/gui/adapter/FileTreeKeyAdapter.java
@@ -0,0 +1,22 @@
+package me.n1ar4.jar.analyzer.gui.adapter;
+
+import me.n1ar4.jar.analyzer.gui.MainForm;
+
+import javax.swing.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class FileTreeKeyAdapter extends KeyAdapter {
+    private final JPanel fileTreeSearchPanel = MainForm.getInstance().getFileTreeSearchPanel();
+    private final JTextField fileTreeSearchtextField = MainForm.getInstance().getFileTreeSearchtextField();
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if ((e.getModifiers() & KeyEvent.CTRL_MASK) != 0 && e.getKeyCode() == KeyEvent.VK_F) {
+            fileTreeSearchPanel.setVisible(!fileTreeSearchPanel.isShowing());
+            if (fileTreeSearchPanel.isShowing()) {
+                fileTreeSearchtextField.requestFocus();
+            }
+        }
+    }
+}

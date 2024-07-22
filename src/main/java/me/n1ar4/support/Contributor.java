9ad027d488706cea37e336fcc09dcb14d7d8cfab
4ra1n <2023503307@qq.com>
2024-07-22 17:17:56 +0800

    CONTRIBUTOR UTIL

--- src/main/java/me/n1ar4/support/Contributor.java
+++ src/main/java/me/n1ar4/support/Contributor.java
@@ -3,22 +3,33 @@ package me.n1ar4.support;
 import com.alibaba.fastjson2.JSONArray;
 import com.alibaba.fastjson2.JSONObject;
 import me.n1ar4.jar.analyzer.core.Proxy;
+import me.n1ar4.jar.analyzer.http.HttpRequest;
 import me.n1ar4.jar.analyzer.http.HttpResponse;
 import me.n1ar4.jar.analyzer.http.Y4Client;
 import me.n1ar4.jar.analyzer.utils.ColorUtil;
 
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
 
-public class Main {
-    public static void main(String[] args) {
-        Y4Client client = new Y4Client();
+public class Contributor {
+    public static void main(String[] args) throws Exception {
         Proxy.setSystemProxy();
-        HttpResponse resp = client.get("https://api.github.com/repos/jar-analyzer/jar-analyzer/contributors");
-        String body = new String(resp.getBody());
-        JSONArray array = JSONArray.parse(body);
-
+        Y4Client client = new Y4Client();
+        HttpRequest req = new HttpRequest();
+        req.setMethod("GET");
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Accept", "application/vnd.github+json");
+        headers.put("Authorization", "Bearer " +
+                new String(Files.readAllBytes(Paths.get("token.txt"))).trim());
+        req.setHeaders(headers);
+        req.setUrl(new URL("https://api.github.com/repos/jar-analyzer/jar-analyzer/contributors"));
+        HttpResponse resp = client.request(req);
+        JSONArray array = JSONArray.parse(new String(resp.getBody(), StandardCharsets.UTF_8));
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < array.size(); i++) {
             JSONObject obj = array.getJSONObject(i);

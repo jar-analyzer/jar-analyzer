0a66e232f42f4d239a0f5c169f5e55878b0409ca
4ra1n <2023503307@qq.com>
2024-08-08 22:51:37 +0800

    update start bat

--- build/start-21.bat
+++ build/start-21.bat
@@ -11,7 +11,7 @@ set "jre_bin_abs="%cur_dir%%jre_bin%""
 
 rem jvm args
 set "other_args=-Dfile.encoding=UTF-8"
-set "java_args=%other_args%"
+set "java_args=-XX:+UseZGC -Xms4g -Xmx4g %other_args%"
 set "java_cp=lib\%jar-analyzer%;lib\tools.jar"
 set "main_class=me.n1ar4.jar.analyzer.starter.Application"
 

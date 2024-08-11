765eeff9df24ea8ead1dd9c2aef29d39d33d6264
4ra1n <2023503307@qq.com>
2024-08-11 13:30:20 +0800

    CLEAN

--- build.py
+++ build.py
@@ -19,12 +19,6 @@ def copy_jar_files(source_dir, target_dir):
                 shutil.copy(source_path, target_path)
 
 
-def copy_config_files(target_dir):
-    source_path = os.path.join("build", "config.yaml")
-    target_path = os.path.join(target_dir, "config.yaml")
-    shutil.copy(source_path, target_path)
-
-
 def copy_exe_files(target_dir):
     source_path = os.path.join("build", "start.exe")
     target_path = os.path.join(target_dir, "start.exe")
@@ -71,9 +65,6 @@ if __name__ == '__main__':
     copy_jar_files(java_target_directory, "{}/{}/{}".format(target_directory, release_win_full_dir, "lib"))
     copy_jar_files(java_target_directory, "{}/{}/{}".format(target_directory, release_win_21_dir, "lib"))
     copy_jar_files(java_target_directory, "{}/{}/{}".format(target_directory, release_zip_dir, "lib"))
-
-    copy_config_files("{}/{}".format(target_directory, release_win_system_dir))
-    copy_config_files("{}/{}".format(target_directory, release_win_full_dir))
 
     copy_exe_files("{}/{}".format(target_directory, release_win_system_dir))
     copy_exe_files("{}/{}".format(target_directory, release_win_full_dir))

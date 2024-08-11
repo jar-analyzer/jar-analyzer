12a536ffbb302f46da3f6e077649c2928e0cf9db
4ra1n <2023503307@qq.com>
2024-08-11 17:10:14 +0800

    UPDATE BAT

--- build/start-21.bat
+++ build/start-21.bat
@@ -12,7 +12,7 @@ set "jre_bin_abs="%cur_dir%%jre_bin%""
 rem jvm args
 set "other_args=-Dfile.encoding=UTF-8"
 set "java_args=-XX:+UseZGC -Xms2g %other_args%"
-set "java_cp=lib\%jar-analyzer%;lib\tools.jar"
+set "java_cp=lib\%jar-analyzer%"
 set "main_class=me.n1ar4.jar.analyzer.starter.Application"
 
 rem support default metal win win-classic motif mac gtk cross aqua nimbus

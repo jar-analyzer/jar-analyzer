7f4bcaa2d8d5c0ff0ab086e63497ba78ce9dda22
4ra1n <2023503307@qq.com>
2024-08-11 17:38:57 +0800

    更新内容 - [优化] 代码区域搜索输入框内容变化实时监听搜索 - [优化] 代码区域搜索按 `ENTER` 键切换下一条记录

--- src/main/java/me/n1ar4/jar/analyzer/gui/SearchForm.java
+++ src/main/java/me/n1ar4/jar/analyzer/gui/SearchForm.java
@@ -7,16 +7,20 @@ import me.n1ar4.jar.analyzer.gui.util.SyntaxAreaHelper;
 import me.n1ar4.jar.analyzer.utils.StringUtil;
 
 import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 public class SearchForm {
     private JPanel rootPanel;
     private JTextField searchText;
-    private JButton searchBtn;
     private JLabel searchLabel;
     private JButton prevButton;
     private JButton nextButton;
     private JLabel resultLabel;
+    private JPanel showPanel;
     private static SearchForm instance;
     private static String searchTextGlobal = null;
     private static int total = 0;
@@ -26,6 +30,7 @@ public class SearchForm {
         instance = new SearchForm();
         instance.init();
         frame.setContentPane(instance.rootPanel);
+        frame.setTitle("SEARCH");
         frame.pack();
         frame.setAlwaysOnTop(true);
         frame.setLocationRelativeTo(MainForm.getInstance().getMasterPanel());
@@ -35,44 +40,90 @@ public class SearchForm {
 
     private void init() {
         resultLabel.setText("0/0");
-        instance.searchBtn.addActionListener(e -> {
-            String text = searchText.getText();
-            // 修复卡死的 BUG
-            if (StringUtil.isNull(text)) {
-                JOptionPane.showMessageDialog(rootPanel, "search text is empty");
-                return;
+
+        instance.searchText.getDocument().addDocumentListener(new DocumentListener() {
+            @Override
+            public void insertUpdate(DocumentEvent e) {
+                updateLabel(e);
             }
-            searchTextGlobal = text;
-            total = SyntaxAreaHelper.addSearchAction(text);
-            if (total == 0) {
-                resultLabel.setText("0/0");
-                return;
+
+            @Override
+            public void removeUpdate(DocumentEvent e) {
+                updateLabel(e);
             }
-            int cur = SyntaxAreaHelper.getCurrentIndex();
-            resultLabel.setText(String.format("%d/%d", cur + 1, total));
+
+            @Override
+            public void changedUpdate(DocumentEvent e) {
+                updateLabel(e);
+            }
+
+            private void updateLabel(DocumentEvent e) {
+                try {
+                    String text = e.getDocument().getText(0, e.getDocument().getLength());
+                    // 修复卡死的 BUG
+                    if (StringUtil.isNull(text)) {
+                        return;
+                    }
+                    searchTextGlobal = text;
+                    total = SyntaxAreaHelper.addSearchAction(text);
+                    if (total == 0) {
+                        resultLabel.setText("0/0");
+                        return;
+                    }
+                    int cur = SyntaxAreaHelper.getCurrentIndex();
+                    resultLabel.setText(String.format("%d/%d", cur + 1, total));
+                } catch (Exception ignored) {
+                }
+            }
         });
+
+        instance.searchText.addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+                    getNext();
+                }
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+            }
+        });
+
         instance.prevButton.addActionListener(e -> {
-            if (searchTextGlobal == null || searchTextGlobal.isEmpty()) {
-                return;
-            }
-            if (total == 0) {
-                return;
-            }
-            SyntaxAreaHelper.navigate(searchTextGlobal, false);
-            int cur = SyntaxAreaHelper.getCurrentIndex();
-            resultLabel.setText(String.format("%d/%d", cur + 1, total));
+            getPrev();
         });
         instance.nextButton.addActionListener(e -> {
-            if (searchTextGlobal == null || searchTextGlobal.isEmpty()) {
-                return;
-            }
-            if (total == 0) {
-                return;
-            }
-            SyntaxAreaHelper.navigate(searchTextGlobal, true);
-            int cur = SyntaxAreaHelper.getCurrentIndex();
-            resultLabel.setText(String.format("%d/%d", cur + 1, total));
+            getNext();
         });
+    }
+
+    private void getNext() {
+        if (searchTextGlobal == null || searchTextGlobal.isEmpty()) {
+            return;
+        }
+        if (total == 0) {
+            return;
+        }
+        SyntaxAreaHelper.navigate(searchTextGlobal, true);
+        int cur = SyntaxAreaHelper.getCurrentIndex();
+        resultLabel.setText(String.format("%d/%d", cur + 1, total));
+    }
+
+    private void getPrev() {
+        if (searchTextGlobal == null || searchTextGlobal.isEmpty()) {
+            return;
+        }
+        if (total == 0) {
+            return;
+        }
+        SyntaxAreaHelper.navigate(searchTextGlobal, false);
+        int cur = SyntaxAreaHelper.getCurrentIndex();
+        resultLabel.setText(String.format("%d/%d", cur + 1, total));
     }
 
     {
@@ -99,21 +150,18 @@ public class SearchForm {
         rootPanel.add(spacer1, new GridConstraints(2, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_VERTICAL, 1, GridConstraints.SIZEPOLICY_WANT_GROW, null, null, null, 0, false));
         searchText = new JTextField();
         rootPanel.add(searchText, new GridConstraints(0, 1, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_FIXED, new Dimension(250, -1), new Dimension(150, -1), null, 0, false));
-        final JPanel panel1 = new JPanel();
-        panel1.setLayout(new GridLayoutManager(1, 4, new Insets(0, 0, 0, 0), -1, -1));
-        rootPanel.add(panel1, new GridConstraints(1, 0, 1, 2, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
-        searchBtn = new JButton();
-        searchBtn.setText("Search");
-        panel1.add(searchBtn, new GridConstraints(0, 3, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        showPanel = new JPanel();
+        showPanel.setLayout(new GridLayoutManager(1, 3, new Insets(0, 0, 0, 0), -1, -1));
+        rootPanel.add(showPanel, new GridConstraints(1, 0, 1, 2, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
         prevButton = new JButton();
         prevButton.setText("Prev");
-        panel1.add(prevButton, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        showPanel.add(prevButton, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
         nextButton = new JButton();
         nextButton.setText("Next");
-        panel1.add(nextButton, new GridConstraints(0, 1, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        showPanel.add(nextButton, new GridConstraints(0, 1, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
         resultLabel = new JLabel();
         resultLabel.setText("0");
-        panel1.add(resultLabel, new GridConstraints(0, 2, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        showPanel.add(resultLabel, new GridConstraints(0, 2, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, new Dimension(80, -1), null, null, 0, false));
     }
 
     /**

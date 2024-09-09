04575b47474ad1518a669b9dacec0fe92609c737
4ra1n <2023503307@qq.com>
2024-09-10 00:40:43 +0800

    [优化] 完善 `Uncaught Exception` 处理

--- src/main/java/me/n1ar4/jar/analyzer/starter/ExpHandler.java
+++ src/main/java/me/n1ar4/jar/analyzer/starter/ExpHandler.java
@@ -27,7 +27,8 @@ package me.n1ar4.jar.analyzer.starter;
 import me.n1ar4.log.LogManager;
 import me.n1ar4.log.Logger;
 
-import java.nio.file.Files;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
 import java.nio.file.Paths;
 
 public class ExpHandler implements Thread.UncaughtExceptionHandler {
@@ -45,16 +46,11 @@ public class ExpHandler implements Thread.UncaughtExceptionHandler {
                 }
             }
             // 处理下异常：不抛出异常 记录到当前目录
-            StringBuilder sb = new StringBuilder();
-            StackTraceElement[] items = e.getStackTrace();
-            for (StackTraceElement item : items) {
-                String info = String.format("%s.%s:%d\n",
-                        item.getClassName(),
-                        item.getMethodName(),
-                        item.getLineNumber());
-                sb.append(info);
-            }
-            Files.write(Paths.get("JAR-ANALYZER-ERROR.txt"), sb.toString().getBytes());
+            FileOutputStream fos = new FileOutputStream(Paths.get("JAR-ANALYZER-ERROR.txt").toFile());
+            PrintWriter ps = new PrintWriter(fos);
+            e.printStackTrace(ps);
+            ps.flush();
+            ps.close();
             logger.error("UNCAUGHT EXCEPTION LOGGED IN JAR-ANALYZER-ERROR.txt");
         } catch (Exception ex) {
             logger.warn("handle thread error: {}", ex.toString());

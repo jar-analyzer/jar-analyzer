c4c955689704ac8228b3db2b66ae7f6736d8bfbb
4ra1n <2023503307@qq.com>
2024-10-19 00:26:16 +0800

    [优化] 优化某些错误代码方法定位时报错的处理 @4ra1n

--- src/main/java/me/n1ar4/jar/analyzer/core/FinderRunner.java
+++ src/main/java/me/n1ar4/jar/analyzer/core/FinderRunner.java
@@ -30,43 +30,52 @@ import com.github.javaparser.ast.body.ConstructorDeclaration;
 import com.github.javaparser.ast.body.InitializerDeclaration;
 import com.github.javaparser.ast.body.MethodDeclaration;
 import com.github.javaparser.ast.expr.SimpleName;
+import me.n1ar4.log.LogManager;
+import me.n1ar4.log.Logger;
 import me.n1ar4.parser.JarAnalyzerParser;
 
 public class FinderRunner {
+    private static final Logger logger = LogManager.getLogger();
+
     public static int find(String total, String methodName, String methodDesc) {
-        // FIX <init> CLASS NAME
-        CompilationUnit cu = JarAnalyzerParser.buildInstance(total);
-        if (methodName.equals("<clinit>")) {
-            InitializerDeclaration id = JarAnalyzerParser.getStaticInitializerDeclaration(cu);
-            if (id == null) {
+        try {
+            // FIX <init> CLASS NAME
+            CompilationUnit cu = JarAnalyzerParser.buildInstance(total);
+            if (methodName.equals("<clinit>")) {
+                InitializerDeclaration id = JarAnalyzerParser.getStaticInitializerDeclaration(cu);
+                if (id == null) {
+                    return 0;
+                }
+                if (id.getBegin().isPresent()) {
+                    return getCur(total, id.getBegin().get());
+                } else {
+                    return 0;
+                }
+            }
+            if (methodName.equals("<init>")) {
+                ConstructorDeclaration cd = JarAnalyzerParser.getConstructor(cu, methodDesc);
+                if (cd == null) {
+                    return 0;
+                }
+                SimpleName sn = cd.getName();
+                if (sn.getBegin().isPresent()) {
+                    return getCur(total, sn.getBegin().get());
+                } else {
+                    return 0;
+                }
+            }
+            MethodDeclaration md = JarAnalyzerParser.getMethod(cu, methodName, methodDesc);
+            if (md == null) {
                 return 0;
             }
-            if (id.getBegin().isPresent()) {
-                return getCur(total, id.getBegin().get());
-            } else {
-                return 0;
-            }
-        }
-        if (methodName.equals("<init>")) {
-            ConstructorDeclaration cd = JarAnalyzerParser.getConstructor(cu, methodDesc);
-            if (cd == null) {
-                return 0;
-            }
-            SimpleName sn = cd.getName();
+            SimpleName sn = md.getName();
             if (sn.getBegin().isPresent()) {
                 return getCur(total, sn.getBegin().get());
             } else {
                 return 0;
             }
-        }
-        MethodDeclaration md = JarAnalyzerParser.getMethod(cu, methodName, methodDesc);
-        if (md == null) {
-            return 0;
-        }
-        SimpleName sn = md.getName();
-        if (sn.getBegin().isPresent()) {
-            return getCur(total, sn.getBegin().get());
-        } else {
+        } catch (Exception ex) {
+            logger.debug("find method position bug: {}", ex.getMessage());
             return 0;
         }
     }

726ffd08c6879061ffbfd2f74555a5251339f56b
4ra1n <2023503307@qq.com>
2024-10-22 23:27:34 +0800

    CODE FORMAT

--- src/main/java/me/n1ar4/jar/analyzer/engine/Index/IndexEngine.java
+++ src/main/java/me/n1ar4/jar/analyzer/engine/Index/IndexEngine.java
@@ -2,6 +2,8 @@ package me.n1ar4.jar.analyzer.engine.Index;
 
 import cn.hutool.core.io.FileUtil;
 import cn.hutool.core.util.StrUtil;
+import me.n1ar4.log.LogManager;
+import me.n1ar4.log.Logger;
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
@@ -23,15 +25,16 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
-
 public class IndexEngine {
-
+    private static final Logger logger = LogManager.getLogger();
 
     private static IndexSearcher searcher;
     private static IndexReader reader;
 
-    public static String addIndexCollection(String documentPath, Map<String, String> analyzerMap) throws IOException {
-        //根据analyzerMap创建索引
+    public static String addIndexCollection(
+            String documentPath,
+            Map<String, String> analyzerMap) throws IOException {
+        // 根据 analyzerMap 创建索引
         Directory directory = FSDirectory.open(Paths.get(documentPath));
         Analyzer analyzer = new StandardAnalyzer();
         IndexWriterConfig conf = new IndexWriterConfig(analyzer);
@@ -42,9 +45,9 @@ public class IndexEngine {
         analyzerMap.forEach((key, value) -> {
             value = StrUtil.cleanBlank(StrUtil.removeAllLineBreaks(value));
             String[] cut = StrUtil.cut(value, 30000);
-            for (int i = 0; i < cut.length; i++) {
+            for (String s : cut) {
                 Document doc = new Document();
-                doc.add(new StringField("content", cut[i], Field.Store.YES));
+                doc.add(new StringField("content", s, Field.Store.YES));
                 doc.add(new StringField("codeName", FileUtil.getName(key), Field.Store.YES));
                 doc.add(new StringField("codePath", key, Field.Store.YES));
                 documents.add(doc);
@@ -81,7 +84,7 @@ public class IndexEngine {
         return false;
     }
 
-    public static boolean addDoucment(String documentPath) {
+    public static boolean addDocument(String documentPath) {
         return false;
     }
 
@@ -91,10 +94,10 @@ public class IndexEngine {
             reader = DirectoryReader.open(directory);
             searcher = new IndexSearcher(reader);
         }
-        keyword= StrUtil.cleanBlank(StrUtil.removeAllLineBreaks(keyword));
+        keyword = StrUtil.cleanBlank(StrUtil.removeAllLineBreaks(keyword));
         WildcardQuery query = new WildcardQuery(new Term("content", "*" + keyword + "*"));
         TopDocs topDocs = searcher.search(query, 10);
-        System.out.println("本次搜索共找到" + topDocs.totalHits + "条数据");
+        logger.info("本次搜索共找到: {} 条数据" + topDocs.totalHits);
 
         ScoreDoc[] scoreDocs = topDocs.scoreDocs;
         for (ScoreDoc scoreDoc : scoreDocs) {
@@ -102,13 +105,12 @@ public class IndexEngine {
             int docID = scoreDoc.doc;
             // 根据编号去找文档
             Document doc = reader.document(docID);
-            System.out.println();
+            // TODO
         }
         return "false";
     }
 
-    public static List<Object> getIndexs() {
+    public static List<Object> getIndexes() {
         return null;
     }
-
 }
--- src/main/java/me/n1ar4/jar/analyzer/engine/Index/IndexPluginsSupport.java
+++ src/main/java/me/n1ar4/jar/analyzer/engine/Index/IndexPluginsSupport.java
@@ -1,7 +1,5 @@
 package me.n1ar4.jar.analyzer.engine.Index;
 
-import cn.hutool.core.date.DateUtil;
-import cn.hutool.core.date.TimeInterval;
 import cn.hutool.core.io.FileUtil;
 import me.n1ar4.jar.analyzer.engine.DecompileEngine;
 import me.n1ar4.jar.analyzer.starter.Const;
@@ -9,36 +7,57 @@ import org.apache.lucene.queryparser.classic.ParseException;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class JarAnalyzerIndexPluginsSupport {
+public class IndexPluginsSupport {
     public final static String CurrentPath = System.getProperty("user.dir");
-    public final static String DoucumentPath = CurrentPath + FileUtil.FILE_SEPARATOR + Const.indexDir;
+    public final static String DocumentPath = CurrentPath + FileUtil.FILE_SEPARATOR + Const.indexDir;
     public final static String TempPath = CurrentPath + FileUtil.FILE_SEPARATOR + Const.tempDir;
 
+    static {
+        Path curPath = Paths.get(CurrentPath);
+        Path indexPath = curPath.resolve(Const.indexDir);
+        Path tempPath = curPath.resolve(Const.tempDir);
+        // MKDIR TEMP DIR
+        if (!Files.exists(tempPath)) {
+            try {
+                Files.createDirectories(tempPath);
+            } catch (Exception ignored) {
+            }
+        }
+        // MKDIR INDEX DIR
+        if (!Files.exists(indexPath)) {
+            try {
+                Files.createDirectories(indexPath);
+            } catch (Exception ignored) {
+            }
+        }
+    }
 
     public static List<File> getJarAnalyzerPluginsSupportAllFiles() {
-        //获取目录中的所有文件，包含子目录，可以排除特定文件，返回File对象列表
-        List<File> files = FileUtil.loopFiles(TempPath, pathname -> {
+        // 获取目录中的所有文件 包含子目录 可以排除特定文件 返回File对象列表
+        return FileUtil.loopFiles(TempPath, pathname -> {
             //只返回.class文件
-            if (pathname.getName().endsWith(".class")) {
-                return true;
-            }
-            return false;
+            return pathname.getName().endsWith(".class");
         });
-        return files;
     }
 
     /**
      * 清除生成标识
      *
      * @param file jar文件
-     * @return
+     * @return code
      */
     public static String getCode(File file) {
         String decompile = DecompileEngine.decompile(file.toPath());
+        if (decompile == null) {
+            return null;
+        }
         return decompile.replace(DecompileEngine.getFERN_PREFIX(), "");
     }
 
@@ -50,12 +69,11 @@ public class JarAnalyzerIndexPluginsSupport {
             String path = file.getPath();
             codeMap.put(path, code);
         });
-        IndexEngine.createIndex(DoucumentPath);
-        IndexEngine.addIndexCollection(DoucumentPath, codeMap);
+        IndexEngine.createIndex(DocumentPath);
+        IndexEngine.addIndexCollection(DocumentPath, codeMap);
     }
 
     public static String search(String keyword) throws IOException, ParseException {
-        return IndexEngine.search(DoucumentPath, keyword);
+        return IndexEngine.search(DocumentPath, keyword);
     }
-
 }

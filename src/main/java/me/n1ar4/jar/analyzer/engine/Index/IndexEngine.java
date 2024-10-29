ce4cc3d8a2920170b4eb464d766dfc2f75928e5c
Honey Baby <1905411470@qq.com>
2024-10-29 11:12:11 +0800

    【优化】索引查询&创建 逻辑优化

--- src/main/java/me/n1ar4/jar/analyzer/engine/Index/IndexEngine.java
+++ src/main/java/me/n1ar4/jar/analyzer/engine/Index/IndexEngine.java
@@ -1,9 +1,7 @@
 package me.n1ar4.jar.analyzer.engine.Index;
 
-import cn.hutool.core.io.FileUtil;
 import cn.hutool.core.util.StrUtil;
-import me.n1ar4.log.LogManager;
-import me.n1ar4.log.Logger;
+import me.n1ar4.jar.analyzer.engine.Index.entity.Result;
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
@@ -26,46 +24,27 @@ import java.util.List;
 import java.util.Map;
 
 public class IndexEngine {
-    private static final Logger logger = LogManager.getLogger();
 
-    private static IndexSearcher searcher;
-    private static IndexReader reader;
-
-    public static String addIndexCollection(
-            String documentPath,
-            Map<String, String> analyzerMap) throws IOException {
-        // 根据 analyzerMap 创建索引
-        Directory directory = FSDirectory.open(Paths.get(documentPath));
-        Analyzer analyzer = new StandardAnalyzer();
-        IndexWriterConfig conf = new IndexWriterConfig(analyzer);
-        conf.setOpenMode(IndexWriterConfig.OpenMode.CREATE);
-        IndexWriter indexWriter = new IndexWriter(directory, conf);
-
+    public static String addIndexCollection(String documentPath, Map<String, String> analyzerMap) throws IOException {
+        IndexWriter indexWriter = IndexSingletonClass.getIndexWriter();
         Collection<Document> documents = new ArrayList<>();
         analyzerMap.forEach((key, value) -> {
-            value = StrUtil.cleanBlank(StrUtil.removeAllLineBreaks(value));
+            value = StrUtil.removeAllLineBreaks(value);
             String[] cut = StrUtil.cut(value, 30000);
-            for (String s : cut) {
+            for (int i = 0; i < cut.length; i++) {
                 Document doc = new Document();
-                doc.add(new StringField("content", s, Field.Store.YES));
-                doc.add(new StringField("codeName", FileUtil.getName(key), Field.Store.YES));
+                doc.add(new StringField("order", i + "", Field.Store.YES));
+                doc.add(new StringField("content", cut[i], Field.Store.YES));
                 doc.add(new StringField("codePath", key, Field.Store.YES));
                 documents.add(doc);
             }
         });
-
         indexWriter.addDocuments(documents);
         indexWriter.commit();
-        indexWriter.close();
         return null;
     }
 
     public static String createIndex(String documentPath) throws IOException {
-        boolean exist = FileUtil.exist(documentPath);
-        if (!exist) {
-            FileUtil.mkdir(documentPath);
-        }
-
         Directory directory = FSDirectory.open(Paths.get(documentPath));
         Analyzer analyzer = new StandardAnalyzer();
         IndexWriterConfig conf = new IndexWriterConfig(analyzer);
@@ -76,41 +55,73 @@ public class IndexEngine {
         return null;
     }
 
-    public static boolean deleteIndex() {
-        return false;
-    }
 
-    public static boolean updateIndex() {
-        return false;
-    }
+    public static Result search(String documentPath, String keyword) throws IOException, ParseException {
+        IndexReader reader = IndexSingletonClass.getReader();
+        keyword = StrUtil.removeAllLineBreaks(keyword);
 
-    public static boolean addDocument(String documentPath) {
-        return false;
-    }
-
-    public static String search(String documentPath, String keyword) throws IOException, ParseException {
-        if (searcher == null && reader == null) {
-            Directory directory = FSDirectory.open(Paths.get(documentPath));
-            reader = DirectoryReader.open(directory);
-            searcher = new IndexSearcher(reader);
-        }
-        keyword = StrUtil.cleanBlank(StrUtil.removeAllLineBreaks(keyword));
         WildcardQuery query = new WildcardQuery(new Term("content", "*" + keyword + "*"));
-        TopDocs topDocs = searcher.search(query, 10);
-        logger.info("本次搜索共找到: {} 条数据" + topDocs.totalHits);
-
+//        keyword = ReUtil.escape(keyword);
+        //RegexpQuery query = new RegexpQuery(new Term("content", ".*" + keyword+".*"));
+        TopDocs topDocs = IndexSingletonClass.getSearcher().search(query, 110);
         ScoreDoc[] scoreDocs = topDocs.scoreDocs;
+        List<Map<String, Object>> arrayList = new ArrayList<>();
         for (ScoreDoc scoreDoc : scoreDocs) {
-            // 取出文档编号
             int docID = scoreDoc.doc;
-            // 根据编号去找文档
             Document doc = reader.document(docID);
-            // TODO
+            Map<String, Object> map = new java.util.HashMap<>();
+            map.put(doc.get("codePath"), doc.get("content"));
+            map.put("order", Integer.parseInt(doc.get("order")));
+            arrayList.add(map);
         }
-        return "false";
+        Result result = new Result();
+        result.setTotal(topDocs.totalHits);
+        result.setData(arrayList);
+        return result;
     }
 
-    public static List<Object> getIndexes() {
-        return null;
+    public static class IndexSingletonClass {
+        private static volatile IndexSearcher searcher = null;
+        private static volatile IndexReader reader = null;
+        private static volatile IndexWriter indexWriter = null;
+
+        public static IndexSearcher getSearcher() throws IOException {
+            if (searcher == null) {
+                synchronized (IndexSingletonClass.class) {
+                    if (searcher == null) {
+                        IndexReader reader1 = getReader();
+                        searcher = new IndexSearcher(reader1);
+                    }
+                }
+            }
+            return searcher;
+        }
+
+        public static IndexReader getReader() throws IOException {
+            if (reader == null) {
+                synchronized (IndexSingletonClass.class) {
+                    if (reader == null) {
+                        Directory directory = FSDirectory.open(Paths.get(IndexPluginsSupport.DocumentPath));
+                        reader = DirectoryReader.open(directory);
+                    }
+                }
+            }
+            return reader;
+        }
+
+        public static IndexWriter getIndexWriter() throws IOException {
+            if (indexWriter == null) {
+                synchronized (IndexSingletonClass.class) {
+                    if (indexWriter == null) {
+                        Directory directory = FSDirectory.open(Paths.get(IndexPluginsSupport.DocumentPath));
+                        Analyzer analyzer = new StandardAnalyzer();
+                        IndexWriterConfig conf = new IndexWriterConfig(analyzer);
+                        conf.setOpenMode(IndexWriterConfig.OpenMode.CREATE);
+                        indexWriter = new IndexWriter(directory, conf);
+                    }
+                }
+            }
+            return indexWriter;
+        }
     }
 }

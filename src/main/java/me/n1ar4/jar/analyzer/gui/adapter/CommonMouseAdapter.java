aa5be246ad943aa2a4a9c2d4cd45785c1b4d0132
4ra1n <2023503307@qq.com>
2025-03-21 00:59:56 +0800

    [功能] 支持不跳转情况下右键预览方法代码

--- src/main/java/me/n1ar4/jar/analyzer/gui/adapter/CommonMouseAdapter.java
+++ src/main/java/me/n1ar4/jar/analyzer/gui/adapter/CommonMouseAdapter.java
@@ -18,6 +18,7 @@ import me.n1ar4.jar.analyzer.entity.ClassResult;
 import me.n1ar4.jar.analyzer.entity.MethodResult;
 import me.n1ar4.jar.analyzer.gui.LuceneSearchForm;
 import me.n1ar4.jar.analyzer.gui.MainForm;
+import me.n1ar4.jar.analyzer.gui.PreviewForm;
 import me.n1ar4.jar.analyzer.gui.state.State;
 import me.n1ar4.jar.analyzer.gui.util.ProcessDialog;
 import me.n1ar4.jar.analyzer.starter.Const;
@@ -39,8 +40,13 @@ import java.util.ArrayList;
 public class CommonMouseAdapter extends MouseAdapter {
     private static final Logger logger = LogManager.getLogger();
 
+    private static JFrame frameIns = null;
+
     @SuppressWarnings("all")
     public void mouseClicked(MouseEvent evt) {
+        if (frameIns != null) {
+            frameIns.dispose();
+        }
         JList<?> list = (JList<?>) evt.getSource();
         // 左键双击
         if (evt.getClickCount() == 2) {
@@ -224,6 +230,56 @@ public class CommonMouseAdapter extends MouseAdapter {
                 Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
                 clipboard.setContents(stringSelection, null);
                 JOptionPane.showMessageDialog(MainForm.getInstance().getMasterPanel(), "COPY OK");
+            });
+
+            JMenuItem previewItem = new JMenuItem("预览 / preview");
+            popupMenu.add(previewItem);
+            previewItem.addActionListener(e -> {
+                MethodResult selectedItem = (MethodResult) list.getSelectedValue();
+                if (selectedItem == null) {
+                    JOptionPane.showMessageDialog(MainForm.getInstance().getMasterPanel(),
+                            "SELECTED METHOD IS NULL");
+                    return;
+                }
+                String className = selectedItem.getClassName();
+                String tempPath = className.replace("/", File.separator);
+                String classPath;
+
+                classPath = String.format("%s%s%s.class", Const.tempDir, File.separator, tempPath);
+                if (!Files.exists(Paths.get(classPath))) {
+                    classPath = String.format("%s%sBOOT-INF%sclasses%s%s.class",
+                            Const.tempDir, File.separator, File.separator, File.separator, tempPath);
+                    if (!Files.exists(Paths.get(classPath))) {
+                        classPath = String.format("%s%sWEB-INF%sclasses%s%s.class",
+                                Const.tempDir, File.separator, File.separator, File.separator, tempPath);
+                        if (!Files.exists(Paths.get(classPath))) {
+                            JOptionPane.showMessageDialog(MainForm.getInstance().getMasterPanel(),
+                                    "<html>" +
+                                            "<p>need dependency or class file not found</p>" +
+                                            "<p>缺少依赖或者文件找不到（考虑加载 rt.jar 并检查你的 JAR 是否合法）</p>" +
+                                            "<p>默认以三种方式找类：</p>" +
+                                            "<p>1.根据类名直接从根目录找（例如 <strong>com/a/b/Demo</strong> ）</p>" +
+                                            "<p>2.从 <strong>BOOT-INF</strong> 找（" +
+                                            "例如 <strong>BOOT-INF/classes/com/a/Demo</strong> ）</p>" +
+                                            "<p>3.从 <strong>WEB-INF</strong> 找（" +
+                                            "例如 <strong>WEB-INF/classes/com/a/Demo</strong> ）<p>" +
+                                            "</html>");
+                            return;
+                        }
+                    }
+                }
+
+                String code = DecompileEngine.decompile(Paths.get(classPath));
+                String methodName = selectedItem.getMethodName();
+
+                int pos = FinderRunner.find(code, methodName, selectedItem.getMethodDesc());
+
+                if (code == null || code.isEmpty()) {
+                    JOptionPane.showMessageDialog(null, "找不到代码无法预览");
+                    return;
+                }
+
+                frameIns = PreviewForm.start(code, pos);
             });
 
             int index = list.locationToIndex(evt.getPoint());

fde7e9900569908158d3bdf9354fdf669e5c05c4
4ra1n <2023503307@qq.com>
2025-04-02 12:30:19 +0800

    [重要] 支持 `Serlvet/Filter/Listener` 静态分析 @4ra1n

--- src/main/java/me/n1ar4/jar/analyzer/core/OtherWebService.java
+++ src/main/java/me/n1ar4/jar/analyzer/core/OtherWebService.java
@@ -1,16 +1,32 @@
 package me.n1ar4.jar.analyzer.core;
 
+import me.n1ar4.jar.analyzer.core.asm.JavaWebClassVisitor;
 import me.n1ar4.jar.analyzer.entity.ClassFileEntity;
+import me.n1ar4.jar.analyzer.starter.Const;
+import me.n1ar4.log.LogManager;
+import me.n1ar4.log.Logger;
+import org.objectweb.asm.ClassReader;
 
 import java.util.ArrayList;
 import java.util.Set;
 
 public class OtherWebService {
+    private static final Logger logger = LogManager.getLogger();
+
     public static void start(
             Set<ClassFileEntity> classFileList,
             ArrayList<String> interceptors,
             ArrayList<String> servlets,
             ArrayList<String> filters,
             ArrayList<String> listeners) {
+        for (ClassFileEntity file : classFileList) {
+            try {
+                JavaWebClassVisitor jcv = new JavaWebClassVisitor(interceptors, servlets, filters, listeners);
+                ClassReader cr = new ClassReader(file.getFile());
+                cr.accept(jcv, Const.AnalyzeASMOptions);
+            } catch (Exception e) {
+                logger.error("error: {}", e.getMessage());
+            }
+        }
     }
 }
---
+++ src/main/java/me/n1ar4/jar/analyzer/core/asm/JavaWebClassVisitor.java
@@ -0,0 +1,71 @@
+package me.n1ar4.jar.analyzer.core.asm;
+
+import me.n1ar4.jar.analyzer.starter.Const;
+import org.objectweb.asm.ClassVisitor;
+
+import java.util.ArrayList;
+
+public class JavaWebClassVisitor extends ClassVisitor {
+    private final ArrayList<String> interceptors;
+    private final ArrayList<String> servlets;
+    private final ArrayList<String> filters;
+    private final ArrayList<String> listeners;
+
+    public JavaWebClassVisitor(
+            ArrayList<String> interceptors,
+            ArrayList<String> servlets,
+            ArrayList<String> filters,
+            ArrayList<String> listeners) {
+        super(Const.ASMVersion);
+        this.interceptors = interceptors;
+        this.servlets = servlets;
+        this.filters = filters;
+        this.listeners = listeners;
+    }
+
+    @Override
+    public void visit(int version, int access, String name, String signature,
+                      String superName, String[] interfaces) {
+        String currentClassName = name.replace('/', '.');
+
+        if (interfaces != null) {
+            for (String iface : interfaces) {
+                String ifaceName = iface.replace('/', '.');
+
+                if (ifaceName.equals("org.springframework.web.servlet.HandlerInterceptor") ||
+                        ifaceName.equals("org.springframework.web.servlet.AsyncHandlerInterceptor")) {
+                    interceptors.add(currentClassName.replace(".", "/"));
+                }
+
+                if (ifaceName.equals("javax.servlet.Servlet") ||
+                        ifaceName.equals("jakarta.servlet.Servlet")) {
+                    servlets.add(currentClassName.replace(".", "/"));
+                }
+
+                if (ifaceName.equals("javax.servlet.Filter") ||
+                        ifaceName.equals("jakarta.servlet.Filter")) {
+                    filters.add(currentClassName.replace(".", "/"));
+                }
+
+                if (ifaceName.equals("javax.servlet.ServletContextListener") ||
+                        ifaceName.equals("jakarta.servlet.ServletContextListener") ||
+                        ifaceName.equals("javax.servlet.ServletRequestListener") ||
+                        ifaceName.equals("jakarta.servlet.ServletRequestListener") ||
+                        ifaceName.equals("javax.servlet.http.HttpSessionListener") ||
+                        ifaceName.equals("jakarta.servlet.http.HttpSessionListener")) {
+                    listeners.add(currentClassName.replace(".", "/"));
+                }
+            }
+        }
+
+        if (superName != null) {
+            String superClassName = superName.replace('/', '.');
+            if (superClassName.equals("javax.servlet.http.HttpServlet") ||
+                    superClassName.equals("jakarta.servlet.http.HttpServlet")) {
+                servlets.add(currentClassName.replace(".", "/"));
+            }
+        }
+
+        super.visit(version, access, name, signature, superName, interfaces);
+    }
+}

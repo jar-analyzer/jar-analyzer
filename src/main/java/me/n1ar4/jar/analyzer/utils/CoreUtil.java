7d40ea2d97a5192c30d4ba5d6ee49ba934bfdc82
4ra1n <2023503307@qq.com>
2025-08-01 22:27:46 +0800

    [优化] 避免解压和构建数据库的过程中创建空目录 @4ra1n

--- src/main/java/me/n1ar4/jar/analyzer/utils/CoreUtil.java
+++ src/main/java/me/n1ar4/jar/analyzer/utils/CoreUtil.java
@@ -16,6 +16,7 @@ import me.n1ar4.log.LogManager;
 import me.n1ar4.log.Logger;
 
 import java.io.IOException;
+import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -39,6 +40,43 @@ public class CoreUtil {
         for (String jarPath : jarPathList) {
             classFileSet.addAll(JarUtil.resolveNormalJarFile(jarPath, jarIdMap.get(jarPath)));
         }
+        // 2025/08/01 解决黑名单生效但是会创建空的目录 误导用户 问题
+        // 遍历 Const.tempDir 目录 如果目录（以及其子目录）里不包含任何文件 删除该目录
+        deleteEmptyDirectories(temp);
         return new ArrayList<>(classFileSet);
+    }
+
+    private static boolean deleteEmptyDirectories(Path directory) {
+        if (!Files.isDirectory(directory)) {
+            return false;
+        }
+
+        boolean isEmpty = true;
+        try {
+            try (DirectoryStream<Path> dirStream = Files.newDirectoryStream(directory)) {
+                for (Path path : dirStream) {
+                    if (Files.isDirectory(path)) {
+                        // 递归检查子目录
+                        boolean childEmpty = deleteEmptyDirectories(path);
+                        if (!childEmpty) {
+                            isEmpty = false;
+                        }
+                    } else {
+                        // 发现文件，目录非空
+                        isEmpty = false;
+                    }
+                }
+            }
+
+            // 如果目录为空，删除它
+            if (isEmpty) {
+                Files.delete(directory);
+            }
+
+            return isEmpty;
+        } catch (IOException e) {
+            logger.error("delete null dir {} error {}", directory, e);
+            return false;
+        }
     }
 }

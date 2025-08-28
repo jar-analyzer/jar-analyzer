f571c654cb9abafbe25cd3ed8a0388155bbab459
4ra1n <2023503307@qq.com>
2025-08-28 21:56:38 +0800

    code format

--- src/main/java/me/n1ar4/jar/analyzer/gui/TaintResultDialog.java
+++ src/main/java/me/n1ar4/jar/analyzer/gui/TaintResultDialog.java
@@ -1,3 +1,13 @@
+/*
+ * GPLv3 License
+ *
+ * Copyright (c) 2023-2025 4ra1n (Jar Analyzer Team)
+ *
+ * This project is distributed under the GPLv3 license.
+ *
+ * https://github.com/jar-analyzer/jar-analyzer/blob/master/LICENSE
+ */
+
 package me.n1ar4.jar.analyzer.gui;
 
 import me.n1ar4.jar.analyzer.core.reference.MethodReference;

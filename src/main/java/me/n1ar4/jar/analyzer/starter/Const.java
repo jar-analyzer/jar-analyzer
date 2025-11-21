84f280989d519a661246e24ae2519be2d94fc910
4ra1n <2023503307@qq.com>
2025-11-21 20:31:53 +0800

    release 5.11

--- src/main/java/me/n1ar4/jar/analyzer/starter/Const.java
+++ src/main/java/me/n1ar4/jar/analyzer/starter/Const.java
@@ -14,7 +14,7 @@ import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.Opcodes;
 
 public interface Const {
-    String version = "5.10";
+    String version = "5.11";
 
     int ASMVersion = Opcodes.ASM9;
 

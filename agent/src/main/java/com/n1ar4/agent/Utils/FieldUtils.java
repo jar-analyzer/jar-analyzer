a4680f4f8cabc6194b8a6851d06631982f2ae281
fantasy <3280907645@qq.com>
2026-01-26 06:15:00 -0800

    fix for Merge Error

--- agent/src/main/java/com/n1ar4/agent/Utils/FieldUtils.java
+++ agent/src/main/java/com/n1ar4/agent/Utils/FieldUtils.java
@@ -1,14 +1,29 @@
 /*
- * GPLv3 License
+ * MIT License
  *
- * Copyright (c) 2022-2026 4ra1n (Jar Analyzer Team)
+ * Copyright (c) 2023-2024 4ra1n (Jar Analyzer Team)
  *
- * This project is distributed under the GPLv3 license.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
  *
- * https://github.com/jar-analyzer/jar-analyzer/blob/master/LICENSE
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 
 package com.n1ar4.agent.Utils;
+
 
 
 import com.agent.vmTool.core.util.StringUtils;

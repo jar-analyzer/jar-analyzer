ca7c3bc65ce346c4bc747a2b1e9ff246e9abc747
4ra1n <2023503307@qq.com>
2026-02-09 18:17:00 +0800

    [其他] 所有依赖升级到支持 `Java 8` 的最新版 @4ra1n

--- check-version.bat
+++ check-version.bat
@@ -1,2 +1,3 @@
 @echo off
+set "JAVA_HOME=C:\\Program Files\\Java\\jdk-1.8"
 mvn versions:display-dependency-updates
--- package.bat
+++ package.bat
@@ -1,5 +1,6 @@
 @echo off
 setlocal
+set "JAVA_HOME=C:\\Program Files\\Java\\jdk-1.8"
 
 set "MAVEN_OPTS=-Dorg.slf4j.simpleLogger.defaultLogLevel=warn"
 mvn -B clean package -DskipTests --file pom.xml

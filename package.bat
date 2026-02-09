f7d232880c8ccb0594bbebd5d8012d314fabd8f9
4ra1n <2023503307@qq.com>
2026-02-09 21:39:26 +0800

    release 5.15

--- package.bat
+++ package.bat
@@ -1,6 +1,5 @@
 @echo off
 setlocal
-set "JAVA_HOME=C:\\Program Files\\Java\\jdk-1.8"
 
 set "MAVEN_OPTS=-Dorg.slf4j.simpleLogger.defaultLogLevel=warn"
 mvn -B clean package -DskipTests --file pom.xml

69ba5026959691d7aeea45fabf5714a299a33878
4ra1n <2023503307@qq.com>
2026-03-18 19:05:20 +0800

    [优化] 取消启动时检测 `Java 8` 版本弹窗提示

--- src/main/java/me/n1ar4/jar/analyzer/starter/Version.java
+++ src/main/java/me/n1ar4/jar/analyzer/starter/Version.java
@@ -18,11 +18,6 @@ import javax.swing.*;
 public class Version {
     private static final Logger logger = LogManager.getLogger();
 
-    public static boolean isJava8() {
-        String version = System.getProperty("java.version");
-        return version.startsWith("1.8");
-    }
-
     public static void check() {
         String version = System.getProperty("java.version");
         if (version.startsWith("1.8")) {
@@ -32,13 +27,6 @@ public class Version {
                     int updateVersion = Integer.parseInt(versionComponents[1]);
                     if (updateVersion <= 191) {
                         logger.warn("risk - java version is lower than 191");
-                        JOptionPane.showMessageDialog(null,
-                                "<html>vulnerability in versions lower than Java <strong>8u191</strong><br>" +
-                                        "please use a higher version<br>" +
-                                        "Java 版本小于 <strong>8u191</strong> 可能存在某些安全漏洞<br>" +
-                                        "建议使用高于 <strong>8u191</strong> 版本的 Java<br>" +
-                                        "该消息只是一个提示（点击确认正常启动）" +
-                                        "</html>");
                     } else {
                         logger.debug("safe - java version is higher than 191");
                     }
@@ -50,13 +38,6 @@ public class Version {
             }
         } else {
             logger.warn("please use java 8 version");
-            JOptionPane.showMessageDialog(null,
-                    "<html>java <strong>8</strong> is recommended<br>your version is <strong>"
-                            + version + "</strong><br>" +
-                            "推荐使用 java <strong>8</strong> 版本<br>你使用的版本是 <strong>"
-                            + version + "</strong><br>" +
-                            "该消息只是一个提示（点击确认正常启动）" +
-                            "</html>");
         }
     }
 }

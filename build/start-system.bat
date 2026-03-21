9e1c052060daa195a0be44b8c8839963df7d8bd5
4ra1n <2023503307@qq.com>
2026-03-21 13:31:36 +0800

    release 5.17

--- build/start-system.bat
+++ build/start-system.bat
@@ -1,7 +1,7 @@
 @echo off
 setlocal
 
-set "jar-analyzer=jar-analyzer-5.16.jar"
+set "jar-analyzer=jar-analyzer-5.17.jar"
 
 rem jvm args
 set "other_args=-Dfile.encoding=UTF-8"
--- src/main/java/me/n1ar4/jar/analyzer/starter/Const.java
+++ src/main/java/me/n1ar4/jar/analyzer/starter/Const.java
@@ -14,7 +14,7 @@ import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.Opcodes;
 
 public interface Const {
-    String version = "5.16";
+    String version = "5.17";
 
     int ASMVersion = Opcodes.ASM9;
 

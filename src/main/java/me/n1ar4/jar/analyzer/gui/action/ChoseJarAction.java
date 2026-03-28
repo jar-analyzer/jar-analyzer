5e7e502cdc143e90fd036bbc75c7c92e1e24282f
4ra1n <2023503307@qq.com>
2026-03-28 11:12:18 +0800

    [优化] 拖拽功能优化支持拖拽到所有地方都可以加载 @4ra1n

--- src/main/java/me/n1ar4/jar/analyzer/gui/action/ChoseJarAction.java
+++ src/main/java/me/n1ar4/jar/analyzer/gui/action/ChoseJarAction.java
@@ -16,12 +16,16 @@ import me.n1ar4.jar.analyzer.gui.util.LogUtil;
 import javax.swing.*;
 import javax.swing.filechooser.FileFilter;
 import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.io.File;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class ChoseJarAction {
     public static void run() {
         MainForm.getInstance().getChoseBtn().addActionListener(e -> {
-            JDialog progressDialog = createProgressDialog();
+            AtomicBoolean cancelled = new AtomicBoolean(false);
+            JDialog progressDialog = createProgressDialog(cancelled);
             progressDialog.setVisible(true);
             new Thread(() -> {
                 JFileChooser fileChooser = new JFileChooser();
@@ -43,6 +47,10 @@ public class ChoseJarAction {
                 });
                 SwingUtilities.invokeLater(() -> {
                     progressDialog.dispose();
+                    if (cancelled.get()) {
+                        LogUtil.info("user cancelled file chooser");
+                        return;
+                    }
                     int option = fileChooser.showOpenDialog(MainForm.getInstance().getMasterPanel());
                     if (option == JFileChooser.APPROVE_OPTION) {
                         File file = fileChooser.getSelectedFile();
@@ -55,16 +63,24 @@ public class ChoseJarAction {
         });
     }
 
-    private static JDialog createProgressDialog() {
+    private static JDialog createProgressDialog(AtomicBoolean cancelled) {
         Window owner = SwingUtilities.getWindowAncestor(MainForm.getInstance().getMasterPanel());
         JDialog dialog = new JDialog(owner);
         dialog.setTitle("提示");
         dialog.setModal(false);
-        dialog.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
+        dialog.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+
+        dialog.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                cancelled.set(true);
+            }
+        });
 
         JLabel label = new JLabel("<html>在最新 <b>Windows</b> 中 " +
                 "<span style='color:#d35400'><b>Java 8</b></span> 打开文件浏览器效率很低，" +
-                "建议使用 <b>Java 21/25</b> 有改进</html>");
+                "建议使用 <b>Java 21/25</b> 有改进" +
+                "<p>关闭此窗口可取消打开 <b>jar-analyzer</b> 支持直接 <b>拖拽</b> 文件到窗口</p></html>");
         JProgressBar bar = new JProgressBar();
         bar.setIndeterminate(true);
 
--- src/main/java/me/n1ar4/jar/analyzer/gui/util/DropHelper.java
+++ src/main/java/me/n1ar4/jar/analyzer/gui/util/DropHelper.java
@@ -14,14 +14,25 @@ import me.n1ar4.jar.analyzer.gui.MainForm;
 import me.n1ar4.log.LogManager;
 import me.n1ar4.log.Logger;
 
+import java.awt.*;
+
 public class DropHelper {
     private static final Logger logger = LogManager.getLogger();
 
     public static void setDrop() {
         MainForm instance = MainForm.getInstance();
-        instance.getMasterPanel().setDropTarget(new DropInstance());
-        instance.getBlackArea().setDropTarget(new DropInstance());
-        MainForm.getCodeArea().setDropTarget(new DropInstance());
+        // 递归为所有子组件设置拖拽目标，实现任意位置拖拽
+        setDropRecursive(instance.getMasterPanel());
         logger.info("set drop target success");
+    }
+
+    private static void setDropRecursive(Component component) {
+        component.setDropTarget(new DropInstance());
+        if (component instanceof Container) {
+            Container container = (Container) component;
+            for (Component child : container.getComponents()) {
+                setDropRecursive(child);
+            }
+        }
     }
 }
